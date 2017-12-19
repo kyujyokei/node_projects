ce6ebb3066f34957af216a82313207f07d14d202
Kejo\Hsieh <kyujyokei@gmail.com>
2017-12-19 15:16:24 -0800

    Initial commit

---
+++ notes-node/app.js
@@ -0,0 +1,15 @@
+console.log('Starting app.js ...');
+
+const fs = require('fs');
+const os = require('os');
+const note = require('./notes.js');
+const _ = require('lodash');
+
+// note.add(9,-2);
+
+console.log(_.uniq([1,2,3,3,1,2,1,5]));
+
+// var user = os.userInfo();
+// fs.appendFile('greetings.txt',`Hello ${user.username}!`);
+
+
---
+++ notes-node/notes.js
@@ -0,0 +1,5 @@
+console.log('Starting notes.js ...');
+
+module.exports.add = (num1,num2) => {
+    console.log(num1 + num2);
+}
