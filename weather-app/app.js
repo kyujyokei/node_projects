e1ce2cc9671ebf483d99f1c2d2e86bb6447e8f9f
kyujyokei <kyujyokei@gmail.com>
2017-12-29 18:21:29 -0800

    Lecture 30

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,6 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=3273%20NW%20Orchard%20Avenue%20Corvallis',
     json: true // tells the data coming back is JSON and will convert to object automatically
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 4));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}, Longitude: ${body.results[0].geometry.location.lng}`);
 });
