c0999276c8e1385d90669548daac360974240501
Kejo\Hsieh <kyujyokei@gmail.com>
2017-12-31 10:22:21 -0800

    Lecture 32

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,33 @@
-var request = require('request');
+const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help','h')
+    .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+console.log(encodedAddress);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=3273%20NW%20Orchard%20Avenue%20Corvallis',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true // tells the data coming back is JSON and will convert to object automatically
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}, Longitude: ${body.results[0].geometry.location.lng}`);
+
+    if (error) {
+        console.log('Unable to connect to Google servers');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find the address');
+    } else if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}, Longitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
